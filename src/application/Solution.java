79c399811ed4938600e064737c3f621f18a7e34f
pierry-savio <pierrysavio.pi@gmail.com>
2025-12-21 07:59:37 -0300

    part #2 - solution: Given a roman numeral, convert it to an integer

--- src/application/Solution.java
+++ src/application/Solution.java
@@ -1,94 +1,90 @@
 package application;
 
 /*
+
+	Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+	Symbol       Value
+	I             1
+	V             5
+	X             10
+	L             50
+	C             100
+	D             500
+	M             1000
+	For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply 
+	X + II. The number 27 is written as XXVII, which is XX + V + II.
+	
+	Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+	Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same
+	principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+	
+	I can be placed before V (5) and X (10) to make 4 and 9. 
+	X can be placed before L (50) and C (100) to make 40 and 90. 
+	C can be placed before D (500) and M (1000) to make 400 and 900.
 	Given a roman numeral, convert it to an integer.
+	
+	Example 1:
+	
+	Input: s = "III"
+	Output: 3
+	Explanation: III = 3.
+	Example 2:
+	
+	Input: s = "LVIII"
+	Output: 58
+	Explanation: L = 50, V= 5, III = 3.
+	Example 3:
+	
+	Input: s = "MCMXCIV"
+	Output: 1994
+	Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+	 
+	
+	Constraints:
+	
+	1 <= s.length <= 15
+	s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+	It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
  */
 
+
 class Solution {
-	
+    
 	public static void main (String args[]) {
 		
-		String s = "III";
+		String input = "MCMXCIV";
+		int output = romanToInt(input);
 		
-		System.out.println("Input: " + s);
-		System.out.println("Output: " + romanToInt(s));
+		System.out.println("Input: " + input);
+		System.out.println("Output: " + output);
 		
 	}
 	
     public static int romanToInt(String s) {
         
         int result = 0;
-        char previousLetter = 'M';
 
         for (int i = 0; i<s.length(); i++){
-    
-            char x = s.charAt(i);
+            
+            char roman = s.charAt(i);
+            char nextRoman = 'n';
 
-            if (taller(x, previousLetter) == x){          
+            if (i < s.length()-1) {
+                nextRoman = s.charAt(i+1);
+            }
 
-                switch (x){
-                    case 'I':
-                        result -= simpleRomanToInteger(previousLetter) + 1;
-                    break;
+            int romanInt = simpleRomanToInteger(roman);
+            int nextRomanInt = simpleRomanToInteger(nextRoman);
 
-                    case 'V':
-                        result -= simpleRomanToInteger(previousLetter) + 5;
-                    break;
-
-                    case 'X':
-                        result -= simpleRomanToInteger(previousLetter) + 10;
-                    break;
-
-                    case 'L':
-                        result -= simpleRomanToInteger(previousLetter) + 50;
-                    break;
-
-                    case 'C':
-                        result -= simpleRomanToInteger(previousLetter) + 100;
-                    break;
-
-                    case 'D':
-                        result -= simpleRomanToInteger(previousLetter) + 500;
-                    break;
-
-                    case 'M':
-                        result -= simpleRomanToInteger(previousLetter) + 1000;
-                    break;
-                }
+            if (romanInt >= nextRomanInt){
+                result += romanInt;
             }
             else{
-                switch (x){
-                    case 'I':
-                        result += 1;
-                    break;
-
-                    case 'V':
-                        result += 5;
-                    break;
-
-                    case 'X':
-                        result += 10;
-                    break;
-
-                    case 'L':
-                        result += 50;
-                    break;
-
-                    case 'C':
-                        result += 100;
-                    break;
-
-                    case 'D':
-                        result += 500;
-                    break;
-
-                    case 'M':
-                        result += 1000;
-                    break;
-                }
-            }
-
-            previousLetter = x;
+                result += nextRomanInt-romanInt;
+                i++;
+            }    
         }
         return result;
     }
@@ -125,89 +121,11 @@ class Solution {
             case 'M':
                 romanInt = 1000;
             break;
-        }
 
+            case 'n':
+                romanInt = 0;
+            break;
+        }
         return romanInt;
-    }
-
-    public static char taller(char first, char second){
-        
-        int firstNumber = 0;
-        int secondNumber = 0;
-        char taller = 'I';
-
-        switch (first){
-
-            case 'I':
-                firstNumber = 1;
-            break;
-
-            case 'V':
-                firstNumber = 5;
-            break;
-
-            case 'X':
-                firstNumber = 10;
-            break;
-
-            case 'L':
-                firstNumber = 50;
-            break;
-
-            case 'C':
-                firstNumber = 100;
-            break;
-
-            case 'D':
-                firstNumber = 500;
-            break;
-
-            case 'M':
-                firstNumber = 1000;
-            break;
-        }
-
-        switch (second){
-
-            case 'I':
-                secondNumber = 1;
-            break;
-
-            case 'V':
-                secondNumber = 5;
-            break;
-
-            case 'X':
-                secondNumber = 10;
-            break;
-
-            case 'L':
-                secondNumber = 50;
-            break;
-
-            case 'C':
-                secondNumber = 100;
-            break;
-
-            case 'D':
-                secondNumber = 500;
-            break;
-
-            case 'M':
-                secondNumber = 1000;
-            break;
-        }
-
-        if (firstNumber > secondNumber){
-            taller = first;
-        }
-        else if (firstNumber < secondNumber){
-            taller = second;
-        }
-        else{
-            taller = 'n';
-        }
-
-        return taller;
     }
 }
