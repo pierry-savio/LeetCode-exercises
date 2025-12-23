5024b321f8c727d3738b3e96b5295f405a1f665f
pierry-savio <pierrysavio.pi@gmail.com>
2025-12-23 09:56:42 -0300

    part #2: Write a function to find the longest common prefix string amongst an array of strings

--- src/application/Solution.java
+++ src/application/Solution.java
@@ -4,18 +4,37 @@ public class Solution {
 
 	public static void main(String[] args) {
 		
+		String[] strs = {"a"};
+		String prefix = longestCommonPrefix(strs);
+		
+		String input = "";
+		
+		for (String s : strs) {
+			input += s + ", ";
+		}
+		
+		input = input.substring(0, input.length()-2);
+		
+		System.out.println("Input: " + input);
+		
+		System.out.println("Output: " + prefix);
 	}
 	
-	public String longestCommonPrefix(String[] strs) {
+	public static String longestCommonPrefix(String[] strs) {
 		
 		String result = "";
+		int count = 0;
 		
 		for (int i = 0; i<strs[0].length(); i++) {
-			if (strs[1].contains(strs[0].substring(0, strs[0].length()-i)) &&
-				strs[2].contains(strs[0].substring(0, strs[0].length()-i))
-			   )
-			{
-				return strs[0].substring(0, strs[0].length()-1);
+			
+			for (int j = 1; j<strs.length; j++) {
+				
+				if (strs[j].contains(strs[0].substring(0, strs[0].length()-i))) {
+					count++;
+				}
+				if (count == strs.length) {
+					return strs[0].substring(0, strs[0].length()-i);
+				}
 			}
 		}
 		return result;
