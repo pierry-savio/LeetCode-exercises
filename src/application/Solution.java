2fad1bc8ac20a2251bf0788161fbffb1317260f8
pierry-savio <pierrysavio.pi@gmail.com>
2026-01-18 15:06:45 -0300

    Accepted: Median of Two Sorted Arrays

--- src/application/Solution.java
+++ src/application/Solution.java
@@ -1,62 +1,57 @@
+/*
+	Median of Two Sorted Arrays:
+
+	Given two sorted arrays nums1 and nums2 of size m and n respectively,
+	return the median of the two sorted arrays.
+
+	The overall run time complexity should be O(log (m+n)).
+	
+	-> Example 1:
+	
+		Input: nums1 = [1,3], nums2 = [2]
+		Output: 2.00000
+		Explanation: merged array = [1,2,3] and median is 2.
+		
+	-> Example 2:
+	
+		Input: nums1 = [1,2], nums2 = [3,4]
+		Output: 2.50000
+		Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+*/
+
 package application;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-	public static void main(String[] args) {
+class Solution {
+	
+	public static void main (String[] args) {
 		
-		String[] strs = {"a"};
-		String prefix = longestCommonPrefix(strs);
+		int[] nums1 = {1,2,8,9,4,6};
+		int[] nums2 = {17,20,18};
 		
-		String input = "";
-		
-		for (String s : strs) {
-			input += s + ", ";
-		}
-		
-		input = input.substring(0, input.length()-2);
-		
-		System.out.println("Input: " + input);
-		
-		System.out.println("Output: " + prefix);
+		System.out.println("Mediana: " + findMedianSortedArrays(nums1, nums2));
 	}
 	
-	public static String longestCommonPrefix(String[] strs) {
-		
-		String result = "";
-		
-        String target = strs[0];
-
-        int count = 0;
-
-        if (strs.length > 1){
-
-            if (target.length() > 1){
-
-                for (int i = 0; i<strs[0].length(); i++){
-
-                    for (int j = 1; j<strs.length; j++){
-
-                        if (strs[j].contains(target.substring(0, target.length()-i))){
-                            count++;
-                        }
-                        else{
-                            count = 0;
-                        }
-                        if (count == strs.length-1){
-                            result = target.substring(0, target.length()-i);
-                        break;
-                        }
-                    }
-                }
-            }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       
+    	List<Integer> nums3 = new ArrayList<>();
+    	
+        for (Integer n1 : nums1) {
+        	nums3.add(n1);
         }
-        else if (strs.length <= 1){
-            result = strs[0];
+        
+        for (Integer n2 : nums2) {
+        	nums3.add(n2);
         }
-        else{
-            result = "";
-        }
-		
-		return result;
-	}
+        
+        Collections.sort(nums3);
+           
+        double centerNumber = nums3.get(nums3.size()/2);
+        double centerNumberMinusOne = (nums3.size()>1) ? nums3.get(nums3.size()/2-1) : 0.0;
+    
+        return (nums3.size()%2 == 0) ? (centerNumber+centerNumberMinusOne)/2 : centerNumber;
+    }
 }
